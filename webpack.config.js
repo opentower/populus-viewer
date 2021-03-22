5a4fbba4dbb7f266fe96023b868294b056dbfbad
Graham Leach-Krouse <gleachkr@gmail.com>
2021-03-22 08:56:07 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path')
+
+module.exports = {
+    output: {
+        library: "Populus",
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        "plugins": [
+                            ["@babel/plugin-transform-react-jsx", {
+                                "pragma": "h",
+                                "pragmaFrag": "Fragment"
+                            }],
+                            "@babel/plugin-proposal-class-properties"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
