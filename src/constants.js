208a9b27485e41ee920713d3a81e29d5f540cf19
Graham Leach-Krouse <gleachkr@gmail.com>
2021-03-29 15:23:34 -0500

    Add pdf upload

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const eventVersion = "org.populus.annotation.4" //increment to start over with a fresh event type
+export const pdfStateType = "org.populus.pdf" //increment to start over with a fresh event type
--- src/welcome.js
+++ src/welcome.js
@@ -1,4 +1,5 @@
-import { h, render, Fragment, Component } from 'preact';
+import { h, createRef, render, Fragment, Component } from 'preact';
+import { pdfStateType }  from "./constants.js"
 import * as Matrix from "matrix-js-sdk"
 
 export default class WelcomeView extends Component {
@@ -19,9 +20,20 @@ class RoomList extends Component {
         this.state = {
             rooms : props.client.getVisibleRooms()
         }
-        props.client.on("Room", _ => {
-            this.setState({ rooms : props.client.getVisibleRooms() })
-        }) //TODO: remove listener on unmount
+        //need to do this to bind "this" as refering to the RoomList component in the listener
+        this.roomListener = this.roomListener.bind(this)
+    }
+
+    roomListener (room) { this.setState({ rooms : this.props.client.getVisibleRooms() }) }
+
+    componentDidMount () { 
+        this.props.client.on("Room", this.roomListener) 
+        this.props.client.on("Room.name", this.roomListener) 
+    }
+
+    componentWillUnmount () { 
+        this.props.client.off("Room", this.roomListener) 
+        this.props.client.off("Room.name", this.roomListener) 
     }
 
     render(props,state) {
@@ -30,6 +42,8 @@ class RoomList extends Component {
         const rooms = state.rooms.map(room => <RoomListing {...props} room={room}/>)
         return (
             <Fragment>
+                <h3>Upload a new PDF</h3>
+                <PdfUpload client={props.client}/>
                 <h3>Current Conversations</h3>
                 <div>{rooms}</div>
             </Fragment>
@@ -74,5 +88,56 @@ class PDFRoomEntry extends Component {
 class AnnotationRoomEntry extends Component {
     render (props, state) {
         return (<div class="roomListingEntry" id={props.room.name}>{props.room.name}</div>)
+    }
+}
+
+class PdfUpload extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    mainForm = createRef()
+
+    fileLoader = createRef()
+     
+    roomNameInput = createRef()
+
+    uploadFile = async (e) => { 
+        e.preventDefault()
+        const theFile = this.fileLoader.current.files[0]
+        const theName = this.roomNameInput.current.value
+        if (theFile.type == "application/pdf") {
+            const id = await this.props.client.createRoom({
+                room_alias_name : theName,
+                visibility : "public",
+                name : theName,
+                topic : "talkin' bout ..." + theName,
+            })
+            this.props.client.uploadContent(theFile).then(e => {
+                    let parts = e.split('/')
+                    this.props.client.sendStateEvent(id.room_id, pdfStateType, {
+                        "identifier": parts[parts.length - 1] 
+                })
+            }).then(_ => this.mainForm.current.reset())
+        }
+    } 
+
+    render (props, state) {
+        return (
+            <form ref={this.mainForm} onsubmit={this.uploadFile}>
+                <div>
+                    <label> Pdf to discuss
+                        <input ref={this.fileLoader}  type="file"/>
+                    </label>
+                </div>
+                <div>
+                    <label> Name to give discussion
+                        <input ref={this.roomNameInput} type="text"></input>
+                    </label>
+                </div>
+                <input type="submit">Create</input>
+            </form>
+        )
     }
 }
