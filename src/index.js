76daffdae92271a036e75583f8140b09ca6ea03b
Graham Leach-Krouse <gleachkr@gmail.com>
2021-04-25 17:19:41 -0500

    Remember to delete focus when leaving pdf

--- src/index.js
+++ src/index.js
@@ -66,7 +66,10 @@ class PopulusViewer extends Component {
 
     pushHistory = newState => {
        if (newState.pdfFocused) this.queryParams.set("title", newState.pdfFocused)
-       if (newState.pdfFocused === null) this.queryParams.delete("title")
+       if (newState.pdfFocused === null) {
+           this.queryParams.delete("title")
+           this.queryParams.delete("focus")
+       }
        if (newState.pageFocused) this.queryParams.set("page", newState.pageFocused)
        if (newState.pageFocused === null) this.queryParams.delete("page")
        this.setState(newState, _ => window.history.pushState({ 
