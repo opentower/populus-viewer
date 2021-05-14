ca42507f8cd3e25211d0612b0bea175f4ddfd990
Graham Leach-Krouse <gleachkr@gmail.com>
2021-05-14 08:51:19 -0500

    use static property rather than init method

--- src/queryParams.js
+++ src/queryParams.js
@@ -1,30 +1,23 @@
 export default class QueryParameters {
-  static tryInit () {
-    if (!QueryParameters.parameters) QueryParameters.parameters = new URLSearchParams(window.location.search)
-  }
+  static parameters = new URLSearchParams(window.location.search)
 
   static get(a) {
-    QueryParameters.tryInit()
     return QueryParameters.parameters.get(a)
   }
 
   static set(a, b) {
-    QueryParameters.tryInit()
     QueryParameters.parameters.set(a, b)
   }
 
   static delete(a) {
-    QueryParameters.tryInit()
     QueryParameters.parameters.delete(a)
   }
 
   static pushHistory(obj) {
-    QueryParameters.tryInit()
     window.history.pushState(obj, '', `?${QueryParameters.parameters.toString()}`)
   }
 
   static replaceHistory(obj) {
-    QueryParameters.tryInit()
     window.history.replaceState(obj, '', `?${QueryParameters.parameters.toString()}`)
   }
 }
