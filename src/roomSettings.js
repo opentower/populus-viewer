b1e57cef10cd93bf986820bc3d3a969fdea90f56
Graham Leach-Krouse <gleachkr@gmail.com>
2021-07-11 09:34:14 -0500

    Allow changes in Room Name via settings

--- src/roomSettings.js
+++ src/roomSettings.js
@@ -8,9 +8,11 @@ export default class RoomSettings extends Component {
   constructor(props) {
     super(props)
     const roomState = props.room.getLiveTimeline().getState(Matrix.EventTimeline.FORWARDS)
-    const currentJoinRule = roomState.getJoinRule()
+    this.initialJoinRule = roomState.getJoinRule()
+    this.initialName = props.room.name
     this.state = {
-      joinRule: currentJoinRule
+      joinRule: this.initialJoinRule,
+      roomName: this.initialName
     }
   }
 
@@ -18,15 +20,25 @@ export default class RoomSettings extends Component {
     this.setState({ joinRule: e.target.value })
   }
 
-  handleSubmit = e => {
-    e.preventDefault()
-    const theContent = { join_rule: this.state.joinRule }
-    Client.client.sendStateEvent(this.props.room.roomId, joinRule, theContent, "")
-      .then(_ => this.props.populateModal(null))
-      .catch(_ => alert("Something went wrong. You may not have permission to adjust some of these settings."))
+  handleNameInput = e => {
+    this.setState({ roomName: e.target.value })
   }
 
-  render(props, state) {
+  handleSubmit = async e => {
+    e.preventDefault()
+    const theContent = { join_rule: this.state.joinRule }
+    const raiseErr = _ => alert("Something went wrong. You may not have permission to adjust some of these settings.")
+    if (this.state.joinRule !== this.initialJoinRule) await Client.client.sendStateEvent(this.props.room.roomId, joinRule, theContent, "").catch(raiseErr)
+    if (this.state.roomName !== this.initialRoomName) await Client.client.setRoomName(this.props.room.roomId, this.state.roomName).catch(raiseErr)
+    this.props.populateModal(null)
+  }
+
+  cancel = e => {
+    e.preventDefault()
+    this.props.populateModal(null)
+  }
+
+  render(_, state) {
     return <Fragment>
       <h3 id="modalHeader">Room Settings</h3>
       <form id="room-settings-form">
@@ -44,8 +56,11 @@ export default class RoomSettings extends Component {
               : "no new members may join the room"
           }
         </div>
+        <label htmlFor="room-name">Room Name</label>
+        <input value={state.roomName} onInput={this.handleNameInput} name="room-name" type="text" />
         <div id="room-settings-submit-wrapper">
           <button className="styled-button" onClick={this.handleSubmit} >Save Changes</button>
+          <button className="styled-button" onClick={this.cancel} >Cancel</button>
         </div>
       </form>
     </Fragment>
