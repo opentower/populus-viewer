63303b727af984b05f52f1cc587f0dce4388d528
Graham Leach-Krouse <gleachkr@gmail.com>
2021-07-15 16:32:24 -0500

    Fix css again

--- src/userPill.js
+++ src/userPill.js
@@ -5,7 +5,7 @@ import './styles/memberPill.css'
 export default function UserPill(props) {
   const colorFromId = new UserColor(props.user.userId)
   return (<Fragment>
-    <span style={{background: colorFromId.light}} class="memberPill">{props.user.displayName}</span>
+    <span style={{background: colorFromId.light}} class="member-pill">{props.user.displayName}</span>
     <wbr />
   </Fragment>
   )
