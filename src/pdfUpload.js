36c6723c062251f71dee727021c6426e8e9f1e0a
Graham Leach-Krouse <gleachkr@gmail.com>
2021-07-19 09:49:48 -0500

    Add input validation to upload form. Closes #52.

--- src/pdfUpload.js
+++ src/pdfUpload.js
@@ -4,6 +4,17 @@ import { roomType, pdfStateType, spaceType, spaceChild } from "./constants.js"
 import Client from './client.js'
 
 export default class PdfUpload extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      querying: false,
+      nameavailable: false,
+      pdfvalid: false
+    }
+  }
+
+  namingTimeout = null
+
   mainForm = createRef()
 
   fileLoader = createRef()
@@ -16,66 +27,96 @@ export default class PdfUpload extends Component {
 
   progressHandler = (progress) => this.setState({progress})
 
-  uploadFile = async (e) => {
-    e.preventDefault()
+  validateName = _ => {
+    clearTimeout(this.namingTimeout)
+    this.setState({querying: true})
+    this.namingTimeout = setTimeout(_ => {
+      Client.client.getRoomIdForAlias(`#${this.toAlias(this.roomNameInput.current.value)}:${Client.client.getDomain()}`)
+        .then(_ => this.setState({querying: false, nameavailable: false}))
+        .catch(err => {
+          if (this.roomNameInput.current.value === "") this.setState({querying: false, nameavailable: false})
+          else if (err.errcode === "M_NOT_FOUND") this.setState({querying: false, nameavailable: true})
+          else alert(err)
+        })
+    }, 1000)
+  }
+
+  validatePdf = _ => {
     const theFile = this.fileLoader.current.files[0]
-    const theName = this.roomNameInput.current.value
-    const theTopic = this.roomTopicInput.current.value
-    if (theFile.type === "application/pdf") {
-      this.submitButton.current.setAttribute("disabled", true)
-      const id = await Client.client.createRoom({
-        room_alias_name: theName.replace(/[\s:]/g, '_'), // replace forbidden characters
-        visibility: "public",
-        name: theName,
-        topic: theTopic,
-        // We declare the room a space
-        creation_content: {
-          [roomType]: spaceType
-        },
-        // we allow anyone to join, by default, for now
-        initial_state: [{
-          type: "m.room.join_rules",
-          state_key: "",
-          content: {join_rule: "public"}
-        }],
-        power_level_content_override: {
-          events: {
-            [spaceChild]: 0 // we allow anyone to annotate, by default, for now
-          }
-        }
-      }).catch(e => { alert(e); })
-      Client.client.uploadContent(theFile, { progressHandler: this.progressHandler }).then(e => {
-        Client.client.sendStateEvent(id.room_id, pdfStateType, { mxc: e })
-        // XXX: this event doesn't get through before the name is
-        // assigned, so the room isn't detected as a pdf room. Probably
-        // need to include the pdf in the room's creation event to make
-        // this work right.
-      }).then(_ => {
-        this.mainForm.current.reset()
-        this.props.showMainView()
-      }).catch(e => alert(e))
-    } else {
+    if (theFile.type === "application/pdf") this.setState({pdfvalid: true})
+    else { 
+      this.setState({pdfvalid: false})
       alert("Please make sure that the file you're uploading is a pdf.")
       this.mainForm.current.reset()
     }
   }
 
-  render () {
+  toAlias(s) { return s.replace(/[\s:]/g, '_') }
+
+  uploadFile = async e => {
+    e.preventDefault()
+    const theFile = this.fileLoader.current.files[0]
+    const theName = this.roomNameInput.current.value
+    const theAlias = this.toAlias(theName)
+    const theTopic = this.roomTopicInput.current.value
+    this.submitButton.current.setAttribute("disabled", true)
+    const id = await Client.client.createRoom({
+      room_alias_name: theAlias, // replace forbidden characters
+      visibility: "public",
+      name: theName,
+      topic: theTopic,
+      // We declare the room a space
+      creation_content: {
+        [roomType]: spaceType
+      },
+      // we allow anyone to join, by default, for now
+      initial_state: [{
+        type: "m.room.join_rules",
+        state_key: "",
+        content: {join_rule: "public"}
+      }],
+      power_level_content_override: {
+        events: {
+          [spaceChild]: 0 // we allow anyone to annotate, by default, for now
+        }
+      }
+    }).catch(e => { alert(e); })
+    Client.client.uploadContent(theFile, { progressHandler: this.progressHandler }).then(e => {
+      Client.client.sendStateEvent(id.room_id, pdfStateType, { mxc: e })
+      // XXX: this event doesn't get through before the name is
+      // assigned, so the room isn't detected as a pdf room. Probably
+      // need to include the pdf in the room's creation event to make
+      // this work right.
+    }).then(_ => {
+      this.mainForm.current.reset()
+      this.props.showMainView()
+    }).catch(e => alert(e))
+  }
+
+  render (_, state) {
     return <form id="pdfUploadForm" ref={this.mainForm} onsubmit={this.uploadFile}>
       <label> Pdf to discuss</label>
-      <input ref={this.fileLoader} accept="application/pdf" type="file" />
+      <input oninput={this.validatePdf} ref={this.fileLoader} accept="application/pdf" type="file" />
       <label>Name for Discussion</label>
-      <input ref={this.roomNameInput} type="text" />
+      <input oninput={this.validateName} ref={this.roomNameInput} type="text" />
+      <div class="pdfupload-form-detail">{
+        state.querying
+          ? "querying..."
+          : state.nameavailable
+            ? "name available"
+            : "name unavailable"
+        }
+      </div>
       <label>Topic of Discussion</label>
       <textarea ref={this.roomTopicInput} type="text" />
       <div id="pdfUploadFormSubmit">
-        <button disabled={this.state.progress} class="styled-button" ref={this.submitButton} type="submit">
-          { this.state.progress ? "Uploading..." : "Create Discussion" }
+        <button disabled={state.progress || state.querying || !state.nameavailable || !state.pdfvalid} class="styled-button" ref={this.submitButton} type="submit">
+          { state.progress ? "Uploading..." : "Create Discussion" }
         </button>
       </div>
-      {this.state.progress
+      {state.progress
         ? <div id="pdfUploadFormProgress">
-          <progress class="styled-progress" max={this.state.progress.total} value={this.state.progress.loaded} />
+          <progress class="styled-progress" max={state.progress.total} value={state.progress.loaded} />
         </div>
         : null
       }
