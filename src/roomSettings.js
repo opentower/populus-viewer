035f80e3a8a12e8f93bc39c3c42c891b5305689c
Graham Leach-Krouse <gleachkr@gmail.com>
2021-07-24 15:24:33 -0500

    Add room visibility to settings menu.

--- src/roomSettings.js
+++ src/roomSettings.js
@@ -10,10 +10,11 @@ export default class RoomSettings extends Component {
     const roomState = props.room.getLiveTimeline().getState(Matrix.EventTimeline.FORWARDS)
     this.initialJoinRule = roomState.getJoinRule()
     this.initialName = props.room.name
+    this.initialVisibility = null
     this.state = {
       joinRule: this.initialJoinRule,
       roomName: this.initialName,
-      visibility: "private"
+      visibility: null
     }
   }
 
@@ -23,6 +24,7 @@ export default class RoomSettings extends Component {
 
   async checkVisibility () {
     const visibility = await Client.client.getRoomDirectoryVisibility(this.props.room.roomId)
+    this.initialVisibility = visibility
     this.setState({visibility: visibility.visibility})
   }
 
@@ -46,6 +48,7 @@ export default class RoomSettings extends Component {
     e.preventDefault()
     const theContent = { join_rule: this.state.joinRule }
     const raiseErr = _ => alert("Something went wrong. You may not have permission to adjust some of these settings.")
+    if (this.state.visibility !== this.initialVisibility) await Client.client.setRoomDirectoryVisibility(this.props.room.roomId, this.state.visibility).catch(raiseErr)
     if (this.state.joinRule !== this.initialJoinRule) await Client.client.sendStateEvent(this.props.room.roomId, joinRule, theContent, "").catch(raiseErr)
     if (this.state.roomName !== this.initialRoomName) await Client.client.setRoomName(this.props.room.roomId, this.state.roomName).catch(raiseErr)
     this.props.populateModal(null)
@@ -57,46 +60,49 @@ export default class RoomSettings extends Component {
   }
 
   render(_, state) {
-    return <Fragment>
-      <h3 id="modalHeader">Room Settings</h3>
-      <form id="room-settings-form">
-        <label htmlFor="visibilty">Visibility:</label>
-        <select class="styled-input" value={state.visibility} name="joinRule" onchange={this.handleVisibilityChange}>
-          <option value="private">Private</option>
-          <option value="public">Publically Listed</option>
-        </select>
-        <div id="room-settings-visibility-info">
-          {state.visibility === "public"
-            ? "the room will appear in public listings"
-            : "the room will be hidden from other users"
-          }
-        </div>
-        <label htmlFor="joinRule">Join Rule:</label>
-        <select value={state.joinRule} name="joinRule" onchange={this.handleJoinRuleChange}>
-          <option value="private">Private</option>
-          <option value="public">Public</option>
-          <option value="invite">Invite-Only</option>
-        </select>
-        <div id="room-settings-join-info">
-          {state.joinRule === "public"
-            ? "anyone who can find the room may join"
-            : state.joinRule === "invite"
-              ? "an explicit invitation is required before joining"
-              : "no new members may join the room"
-          }
-        </div>
-        <label htmlFor="room-name">Room Name</label>
-        <input name="room-name"
-          type="text"
-          class="styled-input"
-          value={state.roomName}
-          onkeydown={this.handleKeydown}
-          onInput={this.handleNameInput} />
-        <div id="room-settings-submit-wrapper">
-          <button className="styled-button" onClick={this.handleSubmit} >Save Changes</button>
-          <button className="styled-button" onClick={this.cancel} >Cancel</button>
-        </div>
-      </form>
-    </Fragment>
+    if (state.visibility) {
+      return <Fragment>
+        <h3 id="modalHeader">Room Settings</h3>
+        <form id="room-settings-form">
+          <label htmlFor="visibilty">Visibility:</label>
+          <select class="styled-input" value={state.visibility} name="joinRule" onchange={this.handleVisibilityChange}>
+            <option value="private">Private</option>
+            <option value="public">Publically Listed</option>
+          </select>
+          <div id="room-settings-visibility-info">
+            {state.visibility === "public"
+              ? "the room will appear in public listings"
+              : "the room will be hidden from other users"
+            }
+          </div>
+          <label htmlFor="joinRule">Join Rule:</label>
+          <select class="styled-input" value={state.joinRule} name="joinRule" onchange={this.handleJoinRuleChange}>
+            <option value="private">Private</option>
+            <option value="public">Public</option>
+            <option value="invite">Invite-Only</option>
+          </select>
+          <div id="room-settings-join-info">
+            {state.joinRule === "public"
+              ? "anyone who can find the room may join"
+              : state.joinRule === "invite"
+                ? "an explicit invitation is required before joining"
+                : "no new members may join the room"
+            }
+          </div>
+          <label htmlFor="room-name">Room Name</label>
+          <input name="room-name"
+            type="text"
+            class="styled-input"
+            value={state.roomName}
+            onkeydown={this.handleKeydown}
+            onInput={this.handleNameInput} />
+          <div id="room-settings-submit-wrapper">
+            <button className="styled-button" onClick={this.handleSubmit} >Save Changes</button>
+            <button className="styled-button" onClick={this.cancel} >Cancel</button>
+          </div>
+        </form>
+      </Fragment>
+    }
+    return <span id="settings-loading">loading...</span>
   }
 }
