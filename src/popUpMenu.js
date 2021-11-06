1c45f7ad4dc6873bf0da842f53c338a38e98634e
Graham Leach-Krouse <gleachkr@gmail.com>
2021-11-06 11:28:03 -0500

    Refactor for multiple completion menu types

--- src/popUpMenu.js
+++ src/popUpMenu.js
@@ -4,20 +4,9 @@ import UserColor from './userColors.js'
 import './styles/popUpMenu.css'
 
 export default class PopupMenu extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      popupItems: [],
-      selection: 0
-    }
-  }
-
   componentDidMount () {
     if (this.props.textarea) {
       this.props.textarea.current.addEventListener("input", this.handleInput)
-      this.props.textarea.current.addEventListener("keydown", this.handleKeydown)
-      this.props.textarea.current.addEventListener("keyup", this.handleKeyup)
       this.props.textarea.current.addEventListener("click", this.cancel)
       this.props.textarea.current.addEventListener("blur", this.cancel)
     }
@@ -26,36 +15,88 @@ export default class PopupMenu extends Component {
   componentWillUnmount () {
     if (this.props.textarea) {
       this.props.textarea.current.removeEventListener("input", this.handleInput)
-      this.props.textarea.current.removeEventListener("keydown", this.handleKeydown)
-      this.props.textarea.current.removeEventListener("keyup", this.handleKeyup)
       this.props.textarea.current.removeEventListener("click", this.cancel)
       this.props.textarea.current.removeEventListener("blur", this.cancel)
     }
   }
 
-  cancel = _ => this.setState({active: false})
+  cancel = _ => this.setState({active: null})
 
   // we use this instead of keydown for compatibility with mobile chrome
   handleInput = e => {
     if (e.data === "@") {
-      this.setState({active: true})
+      this.setState({active: "member"})
+    }
+  }
+
+  insert = s => {
+    const selstart = this.props.textarea.current.selectionStart
+    const selend = this.props.textarea.current.selectionEnd
+    if (selstart === selend) {
+      const initialSegment = this.props.textValue.slice(0, selend)
+      const terminalSegment = this.props.textValue.slice(selend)
+      const newSegment = initialSegment.replace(/@\S*$/, s)
+      this.props.setTextValue(`${newSegment}${terminalSegment}`,
+        _ => {
+          this.props.textarea.current.focus()
+          this.props.textarea.current.selectionEnd = newSegment.length
+        }
+      )
+    }
+    this.cancel()
+  }
+
+  render(props, state) {
+    switch (state.active) {
+      case "member" : return <PopupMenuMembers
+          insert={this.insert}
+          cancel={this.cancel}
+          textarea={props.textarea}
+          textValue={props.textValue}
+          roomId={props.roomId}
+        />
+      default: return null
+    }
+  }
+}
+
+export class PopupMenuMembers extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      popupItems: [],
+      selection: 0
+    }
+  }
+
+  componentDidMount () {
+    if (this.props.textarea) {
+      this.props.textarea.current.addEventListener("keydown", this.handleKeydown)
+      this.props.textarea.current.addEventListener("keyup", this.handleKeyup)
+    }
+  }
+
+  componentWillUnmount () {
+    if (this.props.textarea) {
+      this.props.textarea.current.removeEventListener("keydown", this.handleKeydown)
+      this.props.textarea.current.removeEventListener("keyup", this.handleKeyup)
     }
   }
 
   handleKeydown = e => {
-    if (this.state.active && e.key === "ArrowDown") {
+    if (e.key === "ArrowDown") {
       e.preventDefault()
       if (this.state.selection + 1 < this.state.popupItems.length) {
         this.setState(oldState => { return {selection: oldState.selection + 1} })
       }
     }
-    if (this.state.active && e.key === "ArrowUp") {
+    if (e.key === "ArrowUp") {
       e.preventDefault()
       if (this.state.selection > 0) {
         this.setState(oldState => { return {selection: oldState.selection - 1} })
       }
     }
-    if (this.state.active && e.key === "Enter") {
+    if (e.key === "Enter") {
       e.preventDefault()
       this.insertSelection()
     }
@@ -80,56 +121,34 @@ export default class PopupMenu extends Component {
 
   insertSelection() {
     const userId = this.state.popupItems[this.state.selection].props.member.userId
-    this.insert(`@${userId.split(":")[0].substring(1)} `)
-  }
-
-  insert = s => {
-    const selstart = this.props.textarea.current.selectionStart
-    const selend = this.props.textarea.current.selectionEnd
-    if (selstart === selend) {
-      const initialSegment = this.props.textValue.slice(0, selend)
-      const terminalSegment = this.props.textValue.slice(selend)
-      const newSegment = initialSegment.replace(/@\S*$/, s)
-      this.props.setTextValue(`${newSegment}${terminalSegment}`,
-        _ => {
-          this.props.textarea.current.focus()
-          this.props.textarea.current.selectionEnd = newSegment.length
-        }
-      )
-    }
-    this.setState({active: false})
+    this.props.insert(`@${userId.split(":")[0].substring(1)} `)
   }
 
   handleKeyup = _ => {
-    // TODO: debounce
-    if (this.state.active) {
-      const selstart = this.props.textarea.current.selectionStart
-      const selend = this.props.textarea.current.selectionEnd
-      if (selstart === selend) {
-        const matches = this.props.textValue.slice(0, selend).match(/@\S*$/)
-        if (matches) {
-          const popupItems = this.generatePopupItems(matches[0].substring(1))
-          const newState = {popupItems}
-          if (popupItems.length < this.state.selection + 1) {
-            newState.selection = Math.max(popupItems.length - 1, 0)
-          }
-          this.setState(newState)
-          return
+    const selstart = this.props.textarea.current.selectionStart
+    const selend = this.props.textarea.current.selectionEnd
+    if (selstart === selend) {
+      const matches = this.props.textValue.slice(0, selend).match(/@\S*$/)
+      if (matches) {
+        const popupItems = this.generatePopupItems(matches[0].substring(1))
+        const newState = {popupItems}
+        if (popupItems.length < this.state.selection + 1) {
+          newState.selection = Math.max(popupItems.length - 1, 0)
         }
+        this.setState(newState)
+        return
       }
-      this.setState({active: false})
     }
+    this.props.cancel()
   }
 
   render(_props, state) {
-    if (state.active) {
-      // We use a relatively positioned wrapper to keep the PUM in the document flow
-      return <div style={{top: `${state.popupItems.length * 40}px`}} id="popup-wrapper">
-        <div id="popup-menu">
-          {this.state.popupItems}
-        </div>
+    // We use a relatively positioned wrapper to keep the PUM in the document flow
+    return <div style={{top: `${state.popupItems.length * 40}px`}} id="popup-wrapper">
+      <div id="popup-menu">
+        {this.state.popupItems}
       </div>
-    }
+    </div>
   }
 }
 
