ee72e441ccaebdf555d56afda2d06b39f0a33e98
Graham Leach-Krouse <gleachkr@gmail.com>
2021-11-11 09:45:55 -0600

    Use unread notification count for unread count

--- src/utils/unread.js
+++ src/utils/unread.js
@@ -1,25 +1,8 @@
 import Client from '../client.js'
 
 export function calculateUnread(roomId) {
-  const roomIfJoined = Client.client.getRoom(roomId)
-  if (roomIfJoined) {
-    const events = roomIfJoined.getLiveTimeline().getEvents()
-    const maybeRead = roomIfJoined.getAccountData('m.fully_read')
-    const currentFullyReadId = maybeRead ? maybeRead.getContent().event_id : null
-    let receiptIndex = 0
-    // TODO It'd probably be smarter to start at the most recent and decrement
-    // the counter, rather than what we're doing now
-    if (currentFullyReadId) {
-      for (let i = 0; i < events.length; ++i) {
-        if (currentFullyReadId === events[i].getId()) {
-          receiptIndex = ++i
-          break
-        }
-      }
-    }
-    const unreadEvents = events.slice(receiptIndex).filter(e => e.getType() === 'm.room.message')
-    return unreadEvents.length
-  }
+  const room = Client.client.getRoom(roomId)
+  if (room) return room.getUnreadNotificationCount()
   return "All"
 }
 
