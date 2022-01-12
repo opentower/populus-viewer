b44bff95587d0be77098ff8234c7a2f75b61efff
Graham Leach-Krouse <gleachkr@gmail.com>
2022-01-12 15:56:19 -0600

    Add advanced settings to pdf upload

--- src/pdfUpload.js
+++ src/pdfUpload.js
@@ -8,8 +8,11 @@ export default class PdfUpload extends Component {
     super(props)
     this.state = {
       querying: false,
-      nameavailable: false,
-      pdfvalid: false
+      aliasAvailable: false,
+      pdfvalid: false,
+      name: "",
+      alias: "",
+      details: false
     }
   }
 
@@ -21,6 +24,8 @@ export default class PdfUpload extends Component {
 
   roomNameInput = createRef()
 
+  roomAliasInput = createRef()
+
   roomTopicInput = createRef()
 
   submitButton = createRef()
@@ -29,15 +34,28 @@ export default class PdfUpload extends Component {
 
   keydownHandler = e => e.stopPropagation()
 
-  validateName = _ => {
+  nameInputHandler = e => {
+    e.stopPropagation()
+    this.setState({name: e.target.value}, this.validateAlias)
+  }
+
+  handleToggle = _ => this.setState(oldState => { return { details: !oldState.details } })
+
+  aliasInputHandler = e => {
+    e.stopPropagation()
+    this.setState({alias: this.toAlias(e.target.value)}, this.validateAlias)
+  }
+
+  validateAlias = _ => {
     clearTimeout(this.namingTimeout)
     this.setState({querying: true})
+    const alias = this.state.alias.length > 0 ? this.state.alias : this.toAlias(this.state.name)
     this.namingTimeout = setTimeout(_ => {
-      Client.client.getRoomIdForAlias(`#${this.toAlias(this.roomNameInput.current.value)}:${Client.client.getDomain()}`)
-        .then(_ => this.setState({querying: false, nameavailable: false}))
+      Client.client.getRoomIdForAlias(`#${alias}:${Client.client.getDomain()}`)
+        .then(_ => this.setState({querying: false, aliasAvailable: false}))
         .catch(err => {
-          if (this.roomNameInput.current.value === "") this.setState({querying: false, nameavailable: false})
-          else if (err.errcode === "M_NOT_FOUND") this.setState({querying: false, nameavailable: true})
+          if (alias === "") this.setState({querying: false, aliasAvailable: false})
+          else if (err.errcode === "M_NOT_FOUND") this.setState({querying: false, aliasAvailable: true})
           else alert(err)
         })
     }, 1000)
@@ -61,8 +79,8 @@ export default class PdfUpload extends Component {
   uploadFile = async e => {
     e.preventDefault()
     const theFile = this.fileLoader.current.files[0]
-    const theName = this.roomNameInput.current.value
-    const theAlias = this.toAlias(theName)
+    const theName = this.state.name
+    const theAlias = this.state.alias.length > 0 ? this.state.alias : this.toAlias(this.state.name)
     const theTopic = this.roomTopicInput.current.value
     const mxc = await Client.client.uploadContent(theFile, { progressHandler: this.progressHandler })
     this.submitButton.current.setAttribute("disabled", true)
@@ -109,15 +127,18 @@ export default class PdfUpload extends Component {
         <label for="pdf"> Pdf to discuss</label>
         <input name="pdf" oninput={this.validatePdf} ref={this.fileLoader} accept="application/pdf" type="file" />
         <label for="discussion" >Name for Discussion</label>
-        <input name="discussion" onkeydown={this.keydownHandler} oninput={this.validateName} ref={this.roomNameInput} type="text" />
-        <div class="pdfupload-form-detail">{
-          state.querying
-            ? "querying..."
-            : state.nameavailable
-              ? "name available"
-              : "name unavailable"
-          }
-        </div>
+        <input name="discussion" value={state.name} onkeydown={this.keydownHandler} oninput={this.nameInputHandler} ref={this.roomNameInput} type="text" />
+        {state.details
+          ? null
+          : <div class="pdfupload-form-detail">{
+            state.querying
+              ? "querying..."
+              : state.aliasAvailable
+                ? "name available"
+                : "name unavailable"
+            }
+          </div>
+        }
         <label for="topic">Topic of Discussion</label>
         <textarea
           name="topic"
@@ -126,8 +147,26 @@ export default class PdfUpload extends Component {
           type="text"
           data-gramm="false" // disable grammarly
         />
+        <details open={state.details} ontoggle={this.handleToggle}>
+          <summary>Advanced Settings</summary>
+          <div class="pdfupload-details-wrapper">
+            <label for="discussion" >Canonical Alias</label>
+            <input name="discussion" value={state.alias} placeholder={this.toAlias(state.name)} oninput={this.aliasInputHandler} ref={this.roomAliasInput} type="text" />
+            {!state.details
+              ? null
+              : <div class="pdfupload-form-detail">{
+                state.querying
+                  ? "querying..."
+                  : state.aliasAvailable
+                    ? "alias available"
+                    : "alias unavailable"
+                }
+              </div>
+            }
+          </div>
+        </details>
         <div id="pdfUploadFormSubmit">
-          <button disabled={state.progress || state.querying || !state.nameavailable || !state.pdfvalid} class="styled-button" ref={this.submitButton} type="submit">
+          <button disabled={state.progress || state.querying || !state.aliasAvailable || !state.pdfvalid} class="styled-button" ref={this.submitButton} type="submit">
             { state.progress ? "Uploading..." : "Create Discussion" }
           </button>
         </div>
