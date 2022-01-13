31916315eef903effd71b6326786e3e7ad3f2fa9
Graham Leach-Krouse <gleachkr@gmail.com>
2022-01-13 11:10:49 -0600

    Bail out if processing a malformed link

--- src/links.js
+++ src/links.js
@@ -7,21 +7,23 @@ export function processLinks(elt) {
     const linkArray = Array.from(elt.querySelectorAll("a[href]"))
     linkArray
       .forEach(link => {
-        const url = new URL(link.getAttribute("href"))
-        if (url.host === window.location.host && url.pathname === window.location.pathname) {
-          const hash = new URL(link.getAttribute("href")).hash
-          link.addEventListener("click", e => {
-            e.preventDefault()
-            History.push(hash.slice(1))
-          })
-        } else if (url.host === "matrix.to") {
-          const user = Client.client.getUser(url.hash.slice(2))
-          link.addEventListener("click", e => e.preventDefault()) // do nothing until we have DMS worked out
-          if (user) {
-            const colors = new UserColor(user.userId)
-            link.style.setProperty('--user_dark', colors.dark)
+        try {
+          const url = new URL(link.getAttribute("href"))
+          if (url.host === window.location.host && url.pathname === window.location.pathname) {
+            const hash = new URL(link.getAttribute("href")).hash
+            link.addEventListener("click", e => {
+              e.preventDefault()
+              History.push(hash.slice(1))
+            })
+          } else if (url.host === "matrix.to") {
+            const user = Client.client.getUser(url.hash.slice(2))
+            link.addEventListener("click", e => e.preventDefault()) // do nothing until we have DMS worked out
+            if (user) {
+              const colors = new UserColor(user.userId)
+              link.style.setProperty('--user_dark', colors.dark)
+            }
           }
-        }
+        } catch (e) {}
       })
   }
 }
